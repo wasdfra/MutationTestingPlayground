ac844a7477d897d560172e9b2ab4f3e2e0d83e51
Francisco Azevedo <franciscoazevedo@criticalmanufacturing.com>
2026-06-23 15:16:55 +0100

    Initial commit

---
+++ MutationTestPlayground/MutationTestPlayground/DeeDevBase.cs
@@ -0,0 +1,12 @@
+﻿namespace MutationTestPlayground
+{
+    public abstract class DeeDevBase
+    {
+        public abstract Boolean DeeTestCondition(Dictionary<String, Object> Input);
+
+        public abstract Dictionary<String, Object> DeeActionCode(Dictionary<String, Object> Input);
+
+        public static void UseReference(String assembly, String nameSpace) { }
+
+    }
+}
