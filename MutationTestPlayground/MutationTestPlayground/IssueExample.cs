c2d353563c6e2c25f422b1d8efd1b32efbbec950
Francisco Azevedo <franciscoazevedo@criticalmanufacturing.com>
2026-06-23 15:31:30 +0100

    Added issue example

---
+++ MutationTestPlayground/MutationTestPlayground/IssueExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MutationTestPlayground
+{
+    public class IssueExample : DeeDevBase
+
+    {
+        public override bool DeeTestCondition(Dictionary<string, object> Input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<string, object> DeeActionCode(Dictionary<string, object> Input)
+        {
+            string actionGroupName = Input["ActionGroupName"] as string;
+            string sourceAPI = GetSourceAPIFromActionGroup(actionGroupName);
+
+
+            Input["sourceAPI"] = sourceAPI;
+            return Input;
+
+            string GetSourceAPIFromActionGroup(string actionGroupName)
+            {
+                string sourceAPI = actionGroupName.Split('.')
+                    .Reverse()
+                    .Skip(1)
+                    .FirstOrDefault();
+
+                return sourceAPI;
+            }
+
+        }
+    }
+}
